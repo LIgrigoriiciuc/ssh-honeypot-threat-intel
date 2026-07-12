aa5625442d504d7e33be51ffdab93ca97e72d65f
Luisa Ioana Grigoriiciuc <c4fyssrzzd@privaterelay.appleid.com>
2026-07-13 01:49:29 +0300

    Update honeypot.py

--- honeypot.py
+++ honeypot.py
@@ -4,8 +4,8 @@ def main():
 	try:
 		server_sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		server_sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-		server_sock.bind(('0.0.0.0',2222))
-		server_sock.listen(100)
+		server_sock.bind(('',1024))
+		server_sock.listen(16)
 		input()
 	except socket.error as e:
 		print(f"[error] socket error: {e}")	
