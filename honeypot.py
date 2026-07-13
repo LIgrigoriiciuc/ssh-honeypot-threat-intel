705d0a58987ec4dafca3632b540c5b0468058e0f
LIgrigoriiciuc <c4fyssrzzd@privaterelay.appleid.com>
2026-07-13 18:29:57 +0300

    wrap tcp socket in paramiko ssh transport

--- honeypot.py
+++ honeypot.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 import socket
+import paramiko
 def main():
 	try:
 		server_sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		server_sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-		server_sock.bind(('',1024))
-		server_sock.listen(16)
-		input()
+		server_sock.bind(('',1313))
+		server_sock.listen(13)
+		client_sock, client_addr = server_sock.accept()
+		print(f"connection from {client_addr[0]}, {client_addr[1]}")
+		transport = paramiko.Transport(client_sock)
+		server_key = paramiko.RSAKey.generate(1313)
+		transport.add_server_key(server_key)
+		ssh = paramiko.ServerInterface()
+		transport.start_server(server=ssh)
 	except socket.error as e:
 		print(f"[error] socket error: {e}")	
 if __name__ == "__main__":
