ce2a68fec92592b2aa49f5862a27d31be5e351c6
LIgrigoriiciuc <c4fyssrzzd@privaterelay.appleid.com>
2026-07-17 18:06:52 +0300

    pre-deploy cleanup - thread-safe logging, non-busy wait, fix type imports

--- honeypot.py
+++ honeypot.py
@@ -8,6 +8,8 @@ import uuid
 import json
 import sys
 import logging
+import threading
+lock = threading.Lock()
 server_key = paramiko.RSAKey.from_private_key_file('server_key.pem')
 logging.getLogger("paramiko").setLevel(logging.CRITICAL)
 class SSHServer(paramiko.ServerInterface):
@@ -16,13 +18,17 @@ class SSHServer(paramiko.ServerInterface):
 		self.src_port = client_addr[1]		
 		self.session_id = session_id
 	def check_auth_password(self, username: str, password: str) -> int:
+		lock.acquire()
 		print(json.dumps({"event" : "password_auth", "uuid" : str(self.session_id), "username" : username, "password" : password, "src_ip" : self.src_ip, "src_port" : self.src_port, "ts" : datetime.datetime.now(datetime.timezone.utc).isoformat()}))
+		lock.release()
 		return paramiko.AUTH_FAILED
 		#return super().check_auth_password(username, password)
 	def get_allowed_auths(self, username: str) -> str:
 		return "password,publickey"
 	def check_auth_publickey(self, username: str, key: PKey) -> int:
+		lock.acquire()
 		print(json.dumps({"event" : "publickey_auth", "uuid" : str(self.session_id), "username" : username, "key_name" : key.get_name(), "public_key" : key.get_base64(), "key_fp" : key.fingerprint, "src_ip" : self.src_ip, "src_port" : self.src_port, "ts" : datetime.datetime.now(datetime.timezone.utc).isoformat()}))	
+		lock.release()	
 		return paramiko.AUTH_FAILED 
 def handle_connection(client_sock, client_addr, session_id, time):
 	try:
@@ -33,12 +39,17 @@ def handle_connection(client_sock, client_addr, session_id, time):
 		transport.add_server_key(server_key)
 		ssh = SSHServer(client_addr,session_id)
 		transport.start_server(server=ssh)
+		lock.acquire()
 		print(json.dumps({"event" : "ssh_handshake", "version" : transport.remote_version, "uuid" : str(session_id), "src_ip" : client_addr[0], "src_port" : client_addr[1], "ts" : datetime.datetime.now(datetime.timezone.utc).isoformat()}))
-		while transport.is_active(): 
-			time_module.sleep(0.01)
+		lock.release()
+		transport.join()
+		lock.acquire()
 		print(json.dumps({"event" : "disconnect", "uuid" : str(session_id), "src_ip" : client_addr[0], "src_port" : client_addr[1], "ts" : datetime.datetime.now(datetime.timezone.utc).isoformat(), "spent" : (datetime.datetime.now(datetime.timezone.utc)-time).total_seconds()}))		
+		lock.release()
 	except Exception as e:
+		lock.acquire()
 		print(json.dumps({"event" : "error", "error_msg" : str(e), "uuid" : str(session_id), "src_ip" : client_addr[0], "src_port" : client_addr[1], "ts" : datetime.datetime.now(datetime.timezone.utc).isoformat()}))
+		lock.release()
 def main():
 	server_sock = None
 	session_id = None
@@ -47,17 +58,21 @@ def main():
 		server_sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		server_sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 		server_sock.bind(('',22))
-		server_sock.listen(13)
+		server_sock.listen(128)
 		while True:
 			client_sock, client_addr = server_sock.accept()
 			session_id = uuid.uuid4()
+			lock.acquire()
 			print(json.dumps({"event" : "new_conn", "uuid" : str(session_id), "src_ip" : client_addr[0], "src_port" : client_addr[1], "ts" : datetime.datetime.now(datetime.timezone.utc).isoformat()}))
+			lock.release()	
 			t = threading.Thread(target=handle_connection, args=(client_sock,client_addr, session_id, datetime.datetime.now(datetime.timezone.utc)))
 			t.start()
 	except KeyboardInterrupt:
 		print("shutting down honeypot...")
 	except socket.error as e:
+		lock.acquire()
 		print(json.dumps({"event" : "error", "error_msg" : str(e), "uuid" : str(session_id), "src_ip" : client_addr[0], "src_port" : client_addr[1], "ts" : datetime.datetime.now(datetime.timezone.utc).isoformat()}))
+		lock.release()
 	finally:
 		if server_sock is not None: 
 			server_sock.close()	
